917342495471861fbea7e2c8d18e609fa6c3e01e
unknown <savinpark@data2tech.com>
2024-09-04 08:58:15 +0900

    🐛 Fix: Image Rendering issue

--- src/common/components/preparing/Preparing.view.tsx
+++ src/common/components/preparing/Preparing.view.tsx
@@ -5,16 +5,7 @@ import Image from "next/image";
 const VPreparing: React.FC<IPreparing.IVProps> = (props) => {
   return (
     <div className={styles.preparing}>
-      {/* <Image
-        src={"/hourglass.png"}
-        alt='Preparing...'
-        width={150}
-        height={150}
-        // layout="fill"
-        priority
-        loading='eager'
-      /> */}
-      <img src='/hourglass.png' alt='Preparing...' width={150} height={150} loading='eager' />
+      <img src='/images/hourglass.png' alt='Preparing...' width={150} height={150} loading='eager' />
       <p>Preparing...</p>
     </div>
   );
